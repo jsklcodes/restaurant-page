56d6c311eab7974b0a65e5ba084d1d39ff8bb0dd
Kallyne Layane <hellookallyne@gmail.com>
2024-01-01 18:15:34 -0300

    Add HomePage component

---
+++ src/HomePage.js
@@ -0,0 +1,13 @@
+import Image from './img.jpg';
+
+export const HomePage = () => {
+  return `
+  <h1>Welcome to Brew Haven – Your Perfect Sip Awaits!</h1>
+
+  <img src="${Image}" alt="">
+
+  <h2>🌟 About Us 🌟</h2>
+  <p class="info">At Brew Haven, we believe that every cup of coffee tells a story. Our cozy
+    coffee shop is a haven for coffee enthusiasts and casual sippers alike. From the moment you step through our
+    doors, the rich aroma of freshly ground beans welcomes you to an unparalleled coffee experience.</p>`;
+};
