dc5b72d1a296d05969dc37deef69e67c186b705c
Kallyne Layane <hellookallyne@gmail.com>
2024-01-01 18:22:02 -0300

    Render HomePage content

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import { Header } from './Header.js';
+import { ContentContainer } from './ContentContainer.js';
 import './styles.css';
 
 const contentDiv = document.querySelector('#content');
 
 contentDiv.insertAdjacentHTML('afterbegin', Header());
+
+const header = document.querySelector('.header');
+
+let currentPage = 0;
+
+header.insertAdjacentHTML('afterend', ContentContainer(currentPage));
