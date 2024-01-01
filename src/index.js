b54f2b62bf9ce552e56a975ff40b9fbbf439329e
Kallyne Layane <hellookallyne@gmail.com>
2024-01-01 18:25:53 -0300

    Add logic to navigate between pages

--- src/index.js
+++ src/index.js
@@ -6,8 +6,23 @@ const contentDiv = document.querySelector('#content');
 
 contentDiv.insertAdjacentHTML('afterbegin', Header());
 
+const navButtons = document.querySelectorAll('.navbar a');
 const header = document.querySelector('.header');
 
 let currentPage = 0;
+
+navButtons.forEach(navButton => {
+  navButton.addEventListener('click', event => {
+    const clickedButton = event.currentTarget;
+    currentPage = +clickedButton.dataset.page;
+
+    navButtons.forEach(navButton => navButton.classList.remove('active'));
+
+    clickedButton.classList.add('active');
+
+    header.nextElementSibling.remove();
+    header.insertAdjacentHTML('afterend', ContentContainer(currentPage));
+  });
+});
 
 header.insertAdjacentHTML('afterend', ContentContainer(currentPage));
